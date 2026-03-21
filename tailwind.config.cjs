72f570b99e46b5437bc0e0d2052fe4e556b1b2ca
Jianan <qinxiandiqi@foxmail.com>
2026-03-21 22:55:21 +0800

    fix: change darkMode selector to use .dark class (#1596)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: ["selector", "class"],
+  darkMode: ["selector", ".dark"],
   theme: {
     extend: {
       colors: {
